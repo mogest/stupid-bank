73f90780b7a46d35bcfe24bf2fd817fb99b1b5cf
Mog Nesbitt <mog@seriousorange.com>
2024-05-08 09:22:03 +1200

    Update to latest Rails

--- db/schema.rb
+++ db/schema.rb
@@ -2,16 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_27_081803) do
-
+ActiveRecord::Schema[7.1].define(version: 2019_08_27_081803) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,42 +18,35 @@ ActiveRecord::Schema.define(version: 2019_08_27_081803) do
     t.integer "user_id", null: false
     t.string "account_number", null: false
     t.string "description", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_accounts_on_user_id"
-  end
-
-  create_table "emails", force: :cascade do |t|
-    t.integer "sender_user_id", null: false
-    t.text "message", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "messages", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "subject", null: false
     t.text "body", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "txns", force: :cascade do |t|
     t.integer "account_id", null: false
-    t.datetime "transaction_at", null: false
+    t.datetime "transaction_at", precision: nil, null: false
     t.string "description", null: false
     t.float "amount", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["account_id"], name: "index_txns_on_account_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "pin", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
